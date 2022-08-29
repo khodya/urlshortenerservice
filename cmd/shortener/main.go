7c3a162426946644a5dff0b0e058c3271ce4c0f1
Andrey Khodyrev <akhodyrev@griddynamics.com>
2022-08-28 22:59:33 -0500

    fully implemented

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"fmt"
+	"encoding/base64"
+	"io"
 	"net/http"
+	"net/url"
 )
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,11 +16,51 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "GET")
+	if r.URL.Path == "/" {
+		http.Error(w, "Could not find a shortened url in request.", http.StatusBadRequest)
+		return
+	}
+	println(r.URL.Path)
+	decodedBytes, err := decode(r.URL.Path[1:])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	decoded := string(decodedBytes)
+	if _, err := url.ParseRequestURI(decoded); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Location", decoded)
+	w.WriteHeader(http.StatusTemporaryRedirect)
 }
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "POST")
+	defer r.Body.Close()
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if len(body) == 0 {
+		http.Error(w, "Request body is empty.", http.StatusBadRequest)
+		return
+	}
+	if _, err := url.ParseRequestURI(string(body)); err != nil {
+		http.Error(w, "Could not parse url from request.", http.StatusBadRequest)
+		return
+	}
+	encodedUrl := encode(body)
+	w.WriteHeader(http.StatusCreated)
+	w.Write([]byte(encodedUrl))
+}
+
+func encode(v []byte) string {
+	return base64.StdEncoding.EncodeToString(v)
+}
+
+func decode(v string) ([]byte, error) {
+	return base64.StdEncoding.DecodeString(v)
 }
 
 func main() {
